80b52ce18ae11dfd572faef2ce10438deb9cf330
Amina Balla <aminaballa24@gmail.com>
2026-04-16 23:36:50 +0300

    Add clean weekly report script #4
    This script generates a weekly report of commits by day, showing the number of commits and authors for each day in the past week.

---
+++ git-weekly-report-clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# weekly report script
+
+for i in {0..6}
+do
+  day=$(date -d "$i days ago" +"%d.%m.%Y")
+  commits=$(git log --since="$i days ago" --until="$((i-1)) days ago" --oneline | wc -l)
+  authors=$(git log --since="$i days ago" --until="$((i-1)) days ago" --pretty="%an" | sort | uniq | tr '\n' ',' | sed 's/,$//')
+
+  if [ "$commits" -gt 0 ]; then
+    echo "$day - $commits commits by $authors"
+  fi
+done
